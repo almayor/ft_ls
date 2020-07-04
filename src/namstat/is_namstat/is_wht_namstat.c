36013d59a742aee696bc4d1b64197ff54fba4eed
almayor <mayorovme@gmail.com>
2020-07-04 03:05:32 +0300

    on some systems S_IFWHT isn't defined

--- src/namstat/is_namstat/is_wht_namstat.c
+++ src/namstat/is_namstat/is_wht_namstat.c
@@ -6,13 +6,24 @@
 /*   By: unite <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 23:05:48 by unite             #+#    #+#             */
-/*   Updated: 2020/06/26 04:39:31 by unite            ###   ########.fr       */
+/*   Updated: 2020/07/04 03:05:18 by unite            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+#ifdef S_IFWHT
+
 int	is_wht_namstat(t_namstat *nst)
 {
 	return ((nst->stat.st_mode & S_IFMT) == S_IFWHT);
 }
+
+#else
+
+int	is_wht_namstat(t_namstat *nst)
+{
+	return (0);
+}
+
+#endif
