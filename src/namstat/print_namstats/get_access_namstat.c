7ddc39f9710392d4b423cd252cd853519b3e8cce
almayor <mayorovme@gmail.com>
2020-07-09 16:27:35 +0300

    now setuid, setgid and sticky bit are properly displayed in long format

--- src/namstat/print_namstats/get_access_namstat.c
+++ src/namstat/print_namstats/get_access_namstat.c
@@ -6,41 +6,58 @@
 /*   By: unite <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 23:16:50 by unite             #+#    #+#             */
-/*   Updated: 2020/07/04 04:59:03 by unite            ###   ########.fr       */
+/*   Updated: 2020/07/09 16:26:23 by unite            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static char	get_type_namstat(t_namstat *nst)
+static void	set_type_namstat(t_namstat *nst, char* access)
 {
 	if (is_blk_namstat(nst))
-		return ('b');
-	if (is_chr_namstat(nst))
-		return ('c');
-	if (is_dir_namstat(nst))
-		return ('d');
-	if (is_lnk_namstat(nst))
-		return ('l');
-	if (is_sock_namstat(nst))
-		return ('s');
-	if (is_fifo_namstat(nst))
-		return ('p');
-	return ('-');
+		access[0] = 'b';
+	else if (is_chr_namstat(nst))
+		access[0] = 'c';
+	else if (is_dir_namstat(nst))
+		access[0] = 'd';
+	else if (is_lnk_namstat(nst))
+		access[0] = 'l';
+	else if (is_sock_namstat(nst))
+		access[0] = 's';
+	else if (is_fifo_namstat(nst))
+		access[0] = 'p';
+	else
+		access[0] = '-';
 }
 
-static int	has_acl_namstat(t_namstat *nst)
+static void	set_acl_namstat(t_namstat *nst, char* access)
 {
 	acl_t		acl;
 	acl_entry_t	entry;
 
-	return ((acl = acl_get_link_np(nst->path, ACL_TYPE_EXTENDED)) &&
-			(acl_get_entry(acl, ACL_FIRST_ENTRY, &entry) > 0));
+	if ((acl = acl_get_link_np(nst->path, ACL_TYPE_EXTENDED)) &&
+		(acl_get_entry(acl, ACL_FIRST_ENTRY, &entry) > 0))
+		access[10] = '+';
+	else
+		access[10] = ' ';
 }
 
-static int	has_xattr_namstat(t_namstat *nst)
+static void	set_xattr_namstat(t_namstat *nst, char* access)
 {
-	return (listxattr(nst->path, NULL, 0, XATTR_NOFOLLOW) > 0);
+	if (listxattr(nst->path, NULL, 0, XATTR_NOFOLLOW) > 0)
+		access[10] = '@';
+	else
+		access[10] = ' ';
+}
+
+static void set_flags_namstat(t_namstat *nst, char* access)
+{
+	if (nst->stat.st_mode & S_ISUID)
+		access[3] = access[3] == 'x' ? 's' : 'S';
+	if (nst->stat.st_mode & S_ISGID)
+		access[6] = access[6] == 'x' ? 's' : 'S';
+	if (nst->stat.st_mode & S_ISVTX)
+		access[9] = access[9] == 'x' ? 't' : 'T';
 }
 
 char		*get_access_namstat(t_namstat *nst)
@@ -49,18 +66,15 @@ char		*get_access_namstat(t_namstat *nst)
 	static char	access[12];
 	size_t		i;
 
-	access[0] = get_type_namstat(nst);
 	i = 0;
 	while (i < 9)
 	{
 		access[i + 1] = (nst->stat.st_mode & (1 << (8 - i))) ? bits[i] : '-';
 		i++;
 	}
-	if (has_xattr_namstat(nst))
-		access[10] = '@';
-	else if (has_acl_namstat(nst))
-		access[10] = '+';
-	else
-		access[10] = ' ';
+	set_type_namstat(nst, access);
+	set_acl_namstat(nst, access);
+	set_xattr_namstat(nst, access);
+	set_flags_namstat(nst, access);
 	return (access);
 }
